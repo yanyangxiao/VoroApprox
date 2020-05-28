15480676e767006a2284acb2c9d7eac1dc4a5fbd
yanyangxiao <yanyangxiaoxyy@gmail.com>
2020-05-29 06:51:19 +0800

    Update image_render.cpp

--- renders/image_render.cpp
+++ renders/image_render.cpp
@@ -69,7 +69,7 @@ namespace xyy
 		}
 		else if (channel == 1)
 		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_R8, GL_UNSIGNED_BYTE, data);
 		}
 		glGenerateMipmap(GL_TEXTURE_2D);
 
